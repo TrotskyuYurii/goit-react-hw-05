4b38256b439da0422bd0cd89a2ecb59e44d1dfd1
TrotskyuYurii <tro2001@gmail.com>
2024-04-06 20:41:40 +0300

    detail_beta

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,12 +1,23 @@
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from 'react-router-dom';
+import MovieCast from '../../components/MovieCast/MovieCast';
+import  MovieReviews from '../../components/MovieReviews/MovieReviews';
+
 
 const MovieDetailsPage = () => {
-  
-    const { movieId } = useParams();
-  
-  return (
-    <div>MovieDetailsPage {movieId} </div>
-  )
-}
+  const { movieId } = useParams();
 
-export default MovieDetailsPage
+  return (
+    <div>
+      <p>MovieDetailsPage {movieId}</p>
+      <div>
+        <p>Additional information</p>
+        <ul>
+          <li><NavLink to={`/movies/${movieId}/cast`}>Cast</NavLink></li>
+          <li><NavLink to={`/movies/${movieId}/reviews`}>Reviews</NavLink></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetailsPage;
