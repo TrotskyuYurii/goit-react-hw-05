4572d05b893ea95f2c0cc6043773f1f1095e8838
TrotskyuYurii <tro2001@gmail.com>
2024-04-06 21:25:10 +0300

    backButton

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,21 +1,39 @@
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink, useParams, Route, Routes, useLocation } from 'react-router-dom';
 import MovieCast from '../../components/MovieCast/MovieCast';
-import  MovieReviews from '../../components/MovieReviews/MovieReviews';
-
+import MovieReviews from '../../components/MovieReviews/MovieReviews';
+import { useRef } from 'react';
 
 const MovieDetailsPage = () => {
+
+  const location = useLocation();
+  const backLinkRef = useRef(location.state ?? '/');
   const { movieId } = useParams();
 
   return (
     <div>
-      <p>MovieDetailsPage {movieId}</p>
+      <NavLink to={backLinkRef.current}>⬅Go back</NavLink>
       <div>
-        <p>Additional information</p>
-        <ul>
-          <li><NavLink to={`/movies/${movieId}/cast`}>Cast</NavLink></li>
-          <li><NavLink to={`/movies/${movieId}/reviews`}>Reviews</NavLink></li>
-        </ul>
+        <img src="" alt="" />
+        <div>
+          <p>title</p>
+          <p>UserScore</p>
+          <p>Overview</p>
+          <p>Genres</p>
+        </div>
       </div>
+
+
+      <p>Additional information</p>
+      <ul>
+        <li><NavLink to={`/movies/${movieId}/cast`}>Cast</NavLink></li>
+        <li><NavLink to={`/movies/${movieId}/reviews`}>Reviews</NavLink></li>
+      </ul>
+
+
+      <Routes>
+        <Route path="/movies/:movieId/cast" element={<MovieCast />} />
+        <Route path="/movies/:movieId/reviews" element={<MovieReviews />} />
+      </Routes>
     </div>
   );
 };
